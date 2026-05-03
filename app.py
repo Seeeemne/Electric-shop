386b4e50c12f08ad31ded325bb128743c72f32ba
semen <semengold288@gmail.com>
2026-05-03 14:06:14 +0300

    Configure app for Replit

--- app.py
+++ app.py
@@ -1075,4 +1075,7 @@ def api_order_detail(order_id):
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
